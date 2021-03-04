4ebb1ddeba304618c387cc238f4f0276ae589b53
Vijay <vsvijju987@gmail.com>
2021-03-05 01:18:06 +0530

    using refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,22 @@
 import React, { Component } from "react";
-import Auxiliary from '../../../hoc/Auxiliary';
-import PropTypes from 'prop-types';
+import Auxiliary from "../../../hoc/Auxiliary";
+import PropTypes from "prop-types";
 // import Radium from 'radium';
 // import styled from 'styled-components';
 import classes from "./Person.css";
-import withClass from '../../../hoc/withClass';
+import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
+  constructor(props){
+    super(props)
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount(){
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     return (
       <Auxiliary>
@@ -14,7 +24,13 @@ class Person extends Component {
           I'm a {this.props.name} and i'm {this.props.age} year old
         </p>
         <p>{this.props.children}</p>
-        <input onChange={this.props.change} value={this.props.name} />
+        <input
+          // ref={(inputEl) => this.inputElement = inputEl}
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.change}
+          value={this.props.name}
+        />
       </Auxiliary>
     );
   }
@@ -25,6 +41,6 @@ Person.propTypes = {
   age: PropTypes.number,
   change: PropTypes.func,
   click: PropTypes.func,
-}
+};
 
 export default withClass(Person, classes.Person);
